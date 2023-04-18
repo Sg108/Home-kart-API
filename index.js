ccbd4a39388e1d46c8fbde6784104b46e0b4e09f
Sg108 <68855074+Sg108@users.noreply.github.com>
2023-04-18 14:55:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const paymentRoute = require("./routes/payment")
 const cors = require("cors")
 
 app.use(cors({
-    origin:'https://deft-klepon-a3dbc4.netlify.app',
+    origin:'https://homekart.azurewebsites.net',
     credentials:true
 }))
 app.use(express.json())
